2c28add5ed3a9874ddc151beceddf676208dc44a
Harshjoshi22 <harshjoshi1290@gmail.com>
2026-08-16 18:58:10 +0530

    Fix production authentication cookie

--- Server/utils/generateToken.js
+++ Server/utils/generateToken.js
@@ -8,12 +8,13 @@ export const generateToken = (user, res, message, statuscode = 200) => {
     );
 
     return res
-        .status(statuscode)
-        .cookie("token", token, {
-            httpOnly: true,
-            sameSite: "lax",
-            maxAge: 7 * 24 * 60 * 60 * 1000,
-        })
+    .status(statuscode)
+    .cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+    })
         .json({
             success: true,
             message,
