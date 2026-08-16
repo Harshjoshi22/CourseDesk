6216aeac91d6efecff2ab7a312a8c84e85c16c8d
Harshjoshi22 <harshjoshi1290@gmail.com>
2026-08-16 20:41:07 +0530

    Fix sidebar scrolling

--- Client/src/pages/admin/Sidebar.jsx
+++ Client/src/pages/admin/Sidebar.jsx
@@ -5,25 +5,30 @@ import { Link, Outlet } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div  className="flex">
-      <div   className="hidden  lg:block w-[250px] sm:w-[300px] space-y-8 border-r border-gray-300 dark:border-gray-700 dark:bg-[#141414] p-5 sticky top-0  h-screen">
+    <div className="flex">
+      {/* Sidebar */}
+      <div className="hidden lg:block w-[250px] sm:w-[300px] fixed left-0 top-0 h-screen border-r border-gray-300 dark:border-gray-700 dark:bg-[#141414] p-5">
         <div className="space-y-4 mt-20">
           <Link to="dashboard" className="flex items-center gap-2">
             <ChartNoAxesColumn size={22} />
             <h1>Dashboard</h1>
           </Link>
+
           <Link to="course" className="flex items-center gap-2">
             <SquareLibrary size={22} />
             <h1>Courses</h1>
           </Link>
+
           <Link to="/" className="flex items-center gap-2">
             <Home size={22} />
             <h1>Home</h1>
           </Link>
         </div>
       </div>
-    <div className="flex-1 md:p-24 p-2 dark:bg-gray-200 bg-white">
-        <Outlet/>
+
+      {/* Main content */}
+      <div className="flex-1 lg:ml-[250px] sm:ml-[300px] md:p-24 p-2 dark:bg-gray-200 bg-white min-h-screen">
+        <Outlet />
       </div>
     </div>
   );
